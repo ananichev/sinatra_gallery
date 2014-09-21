ae9d15bde548cb89b7ebe1c2b4c6931e6a0d3a23
Vitalii Ananichev <ananichev@users.noreply.github.com>
2014-09-21 14:01:52 +0300

    Add Image model and veiws.

--- index.rb
+++ index.rb
@@ -1,5 +1,30 @@
-require 'sinatra'
+#database configurations
+configure do
+  Mongoid.configure do |config|
+    name = 'app'
+    config.master = Mongo::Connection.new.db(name)
+    config.persist_in_safe_mode = false
+  end
+end
+
+class Image
+  include Mongoid::Document
+  field :title, type: String
+end
 
 get '/' do
-  "Hello, sinatra!"
-end 
+  @image = Image.all
+  haml :'index'
+end
+
+post '/' do
+  @image = Image.new(title: params[:title])
+  @image.save
+  redirect '/'
+end
+
+get '/image/:id' do
+  @image = Image.find(params[:id])
+  haml :'show'
+end
+
