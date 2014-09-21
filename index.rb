8c0ac67bca43dd65f30592976894c979727bd115
Vitalii Ananichev <ananichev@users.noreply.github.com>
2014-09-21 16:23:10 +0300

    Added functionality of uploader.

--- index.rb
+++ index.rb
@@ -7,24 +7,30 @@ configure do
   end
 end
 
+class ImageUploader < CarrierWave::Uploader::Base
+  storage :file
+end
+
 class Image
   include Mongoid::Document
+  mount_uploader :image, ImageUploader, type: String
   field :title, type: String
 end
 
 get '/' do
-  @image = Image.all
-  haml :'index'
+  @images = Image.all
+  haml :index
 end
 
 post '/' do
   @image = Image.new(title: params[:title])
+  @image.image = params[:image]
   @image.save
   redirect '/'
 end
 
 get '/image/:id' do
   @image = Image.find(params[:id])
-  haml :'show'
+  haml :show
 end
 
